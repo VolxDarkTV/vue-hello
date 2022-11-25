7378528c7e11cb26d111fb317fd0e08e02c1cee1
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-11-25 16:13:56 +0100

    fixed txt

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ createApp({
     return {
         title: 'Hello world',
         text:'text-para',
+        paragraphContainer: 'paragraph-container',
+        testoJsAllineato:'text-allineato',
         circle: 'circle-border',
         immagine: 'img/Vue-png.avif',
 
